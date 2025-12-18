70442160e7c332d19618f4fcbe3de97d9a782eaa
bala-amypo <bala@amypo.in>
2025-12-18 08:50:19 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,40 +1,37 @@
 package com.example.demo.model;
-import jakarta.persistence.*;
 
-@Entity
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-public class Student {
-
+public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @Column(unique=true);
+    @Column(unique=true)
     private String email;
     private String password;
     private String role;
-
     public Long getId() {
         return id;
     }
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
     }
-
-    public String getStumail() {
-        return mail;
+    public String getEmail() {
+        return email;
     }
-    public void setMail(String mail) {
-        this.mail = mail;
+    public void setEmail(String email) {
+        this.email = email;
     }
-
     public String getPassword() {
         return password;
     }
@@ -47,15 +44,14 @@ public class Student {
     public void setRole(String role) {
         this.role = role;
     }
-
-    public User(Long id, String name, String mail, String password,String role) {
+    public User(Long id, String name, String email, String password, String role) {
         this.id = id;
         this.name = name;
-        this.mail = mail;
+        this.email = email;
         this.password = password;
-        this.role=role;
+        this.role = role;
     }
-    public User(){
+    public User() {
     }
     
 }
