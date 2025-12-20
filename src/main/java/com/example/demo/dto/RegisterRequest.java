6da7ccdaddd6630d8a5ccb5bcd8332e4acc9f76c
bala-amypo <bala@amypo.in>
2025-12-20 09:09:40 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -0,0 +1,25 @@
+package com.example.demo.dto;
+
+public class RegisterRequest {
+
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
---
+++ src/main/java/com/example/demo/service/AuthService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.LoginRequest;
+import com.example.demo.dto.RegisterRequest;
+
+public interface AuthService {
+
+    void register(RegisterRequest request);
+
+    String login(LoginRequest request);
+}
