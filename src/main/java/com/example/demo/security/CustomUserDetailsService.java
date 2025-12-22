d85abb7a84e9adb7960cf602c5878ff7f9a3df7b
bala-amypo <bala@amypo.in>
2025-12-22 07:48:15 +0000

    User commit

--- src/main/java/com/example/demo/security/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/security/CustomUserDetailsService.java
@@ -1,30 +1,30 @@
-// package com.example.demo.security;
+package com.example.demo.security;
 
-// import com.example.demo.model.User;
-// import com.example.demo.repository.UserRepository;
-// import java.util.Optional;
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.security.core.userdetails.UserDetailsService;
-// import org.springframework.security.core.userdetails.UsernameNotFoundException;
-// import org.springframework.stereotype.Service;
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import java.util.Optional;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-// @Service
-// public class CustomUserDetailsService implements UserDetailsService {
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
 
-//     private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
-//     public CustomUserDetailsService(UserRepository userRepository) {
-//         this.userRepository = userRepository;
-//     }
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
-//     @Override
-//     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-//         Optional<User> userOpt = userRepository.findByEmail(email);
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> userOpt = userRepository.findByEmail(email);
 
-//         User user = userOpt.orElseThrow(
-//                 () -> new UsernameNotFoundException("User not found")
-//         );
+        User user = userOpt.orElseThrow(
+                () -> new UsernameNotFoundException("User not found")
+        );
 
-//         return new CustomUserDetails(user);
-//     }
-// }
+        return new CustomUserDetails(user);
+    }
+}
