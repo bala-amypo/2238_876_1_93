64a943b06a58648b25d1fdd6d72f88d78f8b3fb6
bala-amypo <bala@amypo.in>
2025-12-23 05:28:51 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,41 +1,41 @@
-package com.example.demo.controller;
+// package com.example.demo.controller;
 
-import com.example.demo.model.User;
-import com.example.demo.service.UserService;
-import com.example.demo.util.JwtUtil;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+// import com.example.demo.model.User;
+// import com.example.demo.service.UserService;
+// import com.example.demo.util.JwtUtil;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/auth")
-public class AuthController {
+// @RestController
+// @RequestMapping("/auth")
+// public class AuthController {
 
-    private final UserService userService;
-    private final JwtUtil jwtUtil;
+//     private final UserService userService;
+//     private final JwtUtil jwtUtil;
 
-    public AuthController(UserService userService, JwtUtil jwtUtil) {
-        this.userService = userService;
-        this.jwtUtil = jwtUtil;
-    }
+//     public AuthController(UserService userService, JwtUtil jwtUtil) {
+//         this.userService = userService;
+//         this.jwtUtil = jwtUtil;
+//     }
 
-    // REGISTER
-    @PostMapping("/register")
-    public ResponseEntity<User> register(@RequestBody User user) {
-        User savedUser = userService.register(user);
-        return ResponseEntity.ok(savedUser);
-    }
+//     // REGISTER
+//     @PostMapping("/register")
+//     public ResponseEntity<User> register(@RequestBody User user) {
+//         User savedUser = userService.register(user);
+//         return ResponseEntity.ok(savedUser);
+//     }
 
-    // LOGIN
-    @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody User user) {
-        User authenticatedUser =
-                userService.login(user.getEmail(), user.getPassword());
+//     // LOGIN
+//     @PostMapping("/login")
+//     public ResponseEntity<String> login(@RequestBody User user) {
+//         User authenticatedUser =
+//                 userService.login(user.getEmail(), user.getPassword());
 
-        String token = jwtUtil.generateToken(
-                authenticatedUser.getEmail(),
-                authenticatedUser.getRole()
-        );
+//         String token = jwtUtil.generateToken(
+//                 authenticatedUser.getEmail(),
+//                 authenticatedUser.getRole()
+//         );
 
-        return ResponseEntity.ok(token);
-    }
-}
+//         return ResponseEntity.ok(token);
+//     }
+// }
