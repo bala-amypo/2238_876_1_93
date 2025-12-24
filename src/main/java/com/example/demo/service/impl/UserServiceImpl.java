15e835f9650cdcc3d7c512e71b3f1606c569ac9c
bala-amypo <bala@amypo.in>
2025-12-24 05:16:35 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -3,13 +3,11 @@ package com.example.demo.service.impl;
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
 
-import java.util.Optional;
-
 public class UserServiceImpl implements UserService {
 
     @Override
-    public Optional<User> findByEmail(String email) {
-        return Optional.empty();
+    public User findByEmail(String email) {
+        return null;
     }
 
     @Override
