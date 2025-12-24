d0f298c80221a8181c0df9fbd9b9b7043a92002c
bala-amypo <bala@amypo.in>
2025-12-24 04:29:26 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,41 +1,32 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.User;
-import com.example.demo.service.UserService;
 import com.example.demo.util.JwtUtil;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final UserService userService;
     private final JwtUtil jwtUtil;
 
-    public AuthController(UserService userService, JwtUtil jwtUtil) {
-        this.userService = userService;
+    public AuthController(JwtUtil jwtUtil) {
         this.jwtUtil = jwtUtil;
     }
 
-    // REGISTER
-    @PostMapping("/register")
-    public ResponseEntity<User> register(@RequestBody User user) {
-        User savedUser = userService.register(user);
-        return ResponseEntity.ok(savedUser);
-    }
-
-    // LOGIN
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody User user) {
-        User authenticatedUser =
-                userService.login(user.getEmail(), user.getPassword());
+    public ResponseEntity<String> login(@RequestParam String username,
+                                        @RequestParam String password) {
 
-        String token = jwtUtil.generateToken(
-                authenticatedUser.getEmail(),
-                authenticatedUser.getRole()
-        );
+        UserDetails userDetails = User.builder()
+                .username(username)
+                .password(password)
+                .roles("USER")
+                .build();
 
+        String token = jwtUtil.generateToken(userDetails);
         return ResponseEntity.ok(token);
     }
 }
